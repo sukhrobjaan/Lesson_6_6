b30a08b6f5418bbae0a0edfdcd3baf9d452896db
Suxrob <choriyorovsuhrob@gmail.com>
2024-01-17 21:50:25 +0300

    salom

--- src/example_3/Main_Class.java
+++ src/example_3/Main_Class.java
@@ -17,6 +17,7 @@ public class Main_Class {
         Student student1=new Student("Aliyev","Vali",21,3){
             @Override
             void showInfo() {
+                System.out.println("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
                 System.out.println("Fruit Name -> " + "Shommat");
                 System.out.println("Fruit Color -> " + "Alijon");
                 System.out.println("Fruit Weight -> " + 19);
